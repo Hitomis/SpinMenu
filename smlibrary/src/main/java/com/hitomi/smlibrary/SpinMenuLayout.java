90220633285cc40487c8212ea85370e751d4c4ca
hitomi <1670957065@qq.com>
2016-09-13 11:16:03 +0800

    Initial commit

---
+++ smlibrary/src/main/java/com/hitomi/smlibrary/SpinMenuLayout.java
@@ -0,0 +1,29 @@
+package com.hitomi.smlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by hitomi on 2016/9/13.
+ */
+public class SpinMenuLayout extends ViewGroup{
+
+
+    public SpinMenuLayout(Context context) {
+        super(context);
+    }
+
+    public SpinMenuLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SpinMenuLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
