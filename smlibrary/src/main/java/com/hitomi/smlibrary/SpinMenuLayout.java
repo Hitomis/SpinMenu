1d90b75fb4f7906db0adff9ce37ad68e38f901d6
hitomi <1670957065@qq.com>
2016-09-14 16:31:06 +0800

    Complete rotation effect

--- smlibrary/src/main/java/com/hitomi/smlibrary/SpinMenuLayout.java
+++ smlibrary/src/main/java/com/hitomi/smlibrary/SpinMenuLayout.java
@@ -1,16 +1,17 @@
 package com.hitomi.smlibrary;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 
 /**
  * Created by hitomi on 2016/9/13.
  */
-public class SpinMenuLayout extends ViewGroup {
+public class SpinMenuLayout extends ViewGroup implements Runnable{
 
     /**
      * 计算半径的比例系数
@@ -22,13 +23,30 @@ public class SpinMenuLayout extends ViewGroup {
      */
     private static final int ANGEL_SPACE = 45;
 
-    private VelocityTracker vTracker;
+    /**
+     * 点击与触摸的切换阀值
+     */
+    private int touchSlop = 8;
 
-    private float startAngle, delayAngle;
+    private float delayAngle, perAngle;
 
     private float radius;
 
     private float preX, preY;
+
+    private long preTimes;
+
+    /**
+     * 是否顺时针
+     */
+    private boolean wise;
+
+    /**
+     * 是否正在自动滚动
+     */
+    private boolean isFling;
+
+    private float anglePerSecond;
 
     public SpinMenuLayout(Context context) {
         this(context, null);
@@ -40,13 +58,17 @@ public class SpinMenuLayout extends ViewGroup {
 
     public SpinMenuLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+            ViewConfiguration conf = ViewConfiguration.get(getContext());
+            touchSlop = conf.getScaledTouchSlop();
+        }
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(Math.min(width, height), Math.max(width, height));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         if (getChildCount() > 0) {
             // 对子元素进行测量
@@ -60,7 +82,7 @@ public class SpinMenuLayout extends ViewGroup {
         if (childCount <= 0) return;
 
         delayAngle %= 360;
-        startAngle = delayAngle;
+        float startAngle = delayAngle;
 
         View child;
         int childWidth, childHeight;
@@ -85,6 +107,25 @@ public class SpinMenuLayout extends ViewGroup {
     }
 
     @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        float curX = ev.getX();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                preX = curX;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 当手指拖动值大于 TouchSlop 值时，认为应该进行滚动，拦截子控件的事件
+                float diffX = Math.abs(curX - preX);
+                preX = curX;
+                if (diffX > touchSlop) {
+                    return true;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
 
         float curX = ev.getX();
@@ -94,39 +135,80 @@ public class SpinMenuLayout extends ViewGroup {
             case MotionEvent.ACTION_DOWN:
                 preX = curX;
                 preY = curY;
+                preTimes = System.currentTimeMillis();
+                perAngle = 0;
 
-                if (vTracker == null) {
-                    vTracker = VelocityTracker.obtain();
-                } else {
-                    vTracker.clear();
+                if (isFling) {
+                    // 移除快速滚动的回调
+                    removeCallbacks(this);
+                    isFling = false;
+                    return true;
                 }
-                vTracker.addMovement(ev);
                 break;
             case MotionEvent.ACTION_MOVE:
-                vTracker.addMovement(ev);
-                vTracker.computeCurrentVelocity(1000);
-
+                float diffX = curX - preX;
+                if (Math.abs(diffX) < touchSlop) break;
                 float start = getAngle(preX, preY);
                 float end = getAngle(curX, curY);
-                delayAngle += start - end;
+
+                if (diffX > 0) {
+                    wise = true;
+                    delayAngle += Math.abs(start - end);
+                    perAngle += Math.abs(start - end);
+                } else {
+                    wise = false;
+                    delayAngle -= Math.abs(end - start);
+                    perAngle -= Math.abs(end - start);
+                }
+
                 requestLayout();
 
                 preX = curX;
                 preY = curY;
                 break;
             case MotionEvent.ACTION_UP:
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                vTracker.recycle();
+                anglePerSecond = perAngle * 1000 / (System.currentTimeMillis() - preTimes);
+                if (Math.abs(anglePerSecond) > 100 && !isFling) {
+                    // post一个任务，去自动滚动
+                    post(this);
+                    return true;
+                }
+                if (Math.abs(perAngle) > 3) {
+                    return true;
+                }
                 break;
         }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
         return true;
     }
 
     private float getAngle(float xTouch, float yTouch) {
-        double x = Math.abs(getMeasuredWidth() - xTouch - radius);
-        double y = Math.abs(getMeasuredHeight() - yTouch - radius);
+        // 圆心点在底边的中点上，根据圆心点转化为对应坐标x, y
+        float x = Math.abs(xTouch - getMeasuredWidth() / 2);
+        float y = Math.abs(getMeasuredHeight() - yTouch);
         return (float) (Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI);
     }
 
+
+    @Override
+    public void run() {
+        if (Math.abs(anglePerSecond) < 20) {
+            isFling = false;
+            return;
+        }
+        isFling = true;
+        if (wise) {
+            delayAngle += (Math.abs(anglePerSecond) / 15);
+        } else {
+            delayAngle -= (Math.abs(anglePerSecond) / 15);
+        }
+
+        anglePerSecond /= 1.1f;
+        postDelayed(this, 15);
+        requestLayout();
+    }
 }
