24bd5744d25d19ee1993623216841cdaf26e0c42
hitomi <1670957065@qq.com>
2016-09-21 11:53:24 +0800

    Fixed onSpinMenuStateChangeListener

---
+++ smlibrary/src/main/java/com/hitomi/smlibrary/OnSpinMenuStateChangeListener.java
@@ -0,0 +1,13 @@
+package com.hitomi.smlibrary;
+
+/**
+ * 菜单状态改变时（打开或关闭）的回调接口
+ * Created by hitomi on 2016/9/21.
+ */
+public interface OnSpinMenuStateChangeListener {
+
+    void onMenuOpened();
+
+    void onMenuClosed();
+
+}
